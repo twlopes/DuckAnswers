0c5ebaa86c5decae306a9a34d8f74fa5879512bb
Tristan Lopes <tristan.lopes@neonstingray.com>
2014-06-26 17:23:21 +1000

    Initial Commit

---
+++ DuckAnswers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuckAnswers
+//
+//  Created by Tristan Lopes on 26/06/2014.
+//  Copyright (c) 2014 twlopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
